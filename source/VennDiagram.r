33f624668544e4a867cd3b082dace9906410c486
renyiwu <renyiwu@gmail.com>
2017-12-11 18:12:04 -0500

    Add codes for 4 sets.

--- source/VennDiagram.r
+++ source/VennDiagram.r
@@ -1,4 +1,5 @@
 #Drew Venn Diagram with all values 
+#install.packages("VennDiagram")
 require(VennDiagram)
 venn.plot <- draw.triple.venn(
   area1 = 49,
@@ -19,3 +20,35 @@ venn.plot <- draw.triple.venn(
 tiff(filename = "test3.tiff", compression = "none",type = "windows",antialias = "none")
 grid.draw(venn.plot)
 dev.off()
+
+#Draw a Venn Diagram with 4 sets:
+venn.plot <- draw.quad.venn(
+  area1 = 72,
+  area2 = 86,
+  area3 = 50,
+  area4 = 52,
+  n12 = 44,
+  n13 = 27,
+  n14 = 32,
+  n23 = 38,
+  n24 = 32,
+  n34 = 20,
+  n123 = 18,
+  n124 = 17,
+  n134 = 11,
+  n234 = 13,
+  n1234 = 6,
+  category = c("First", "Second", "Third", "Fourth"),
+  fill = c("orange", "red", "green", "blue"),
+  lty = "dashed",
+  cex = 2,
+  cat.cex = 2,
+  cat.col = c("orange", "red", "green", "blue")
+);
+# Writing to file
+tiff(filename = "Quad_Venn_diagram.tiff", compression = "lzw");
+grid.draw(venn.plot);
+dev.off()
+
+#And more, see http://www.stats.bris.ac.uk/R/web/packages/VennDiagram/VennDiagram.pdf
+
