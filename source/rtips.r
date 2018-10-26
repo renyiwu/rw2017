13f95efd99c54e0ec4419efd0987f8b09576e0c0
renyiwu <renyiwu@gmail.com>
2018-10-26 13:03:40 -0400

    10-26

--- source/rtips.r
+++ source/rtips.r
@@ -7,7 +7,7 @@
 
 
 # 1, sort a data.frame based on columns. (https://stackoverflow.com/questions/1296646/how-to-sort-a-dataframe-by-columns)
-dd <- data.frame(b = factor(c("Hi", "Med", "Hi", "Low"), 
+dd <- data.frame(b = factor(c("Hi", "Med", "Hi", "Low"),
       levels = c("Low", "Med", "Hi"), ordered = TRUE),
       x = c("A", "D", "A", "C"), y = c(8, 3, 9, 9),
       z = c(1, 1, 1, 2))
@@ -26,25 +26,207 @@ dd[ order(-dd[,4], dd[,1]), ]
 
 # 2. data.table tips.
 library("data.table")
-load("~/Downloads/ghData/datasets_L07.Rda")
+load("source/kb/ghData/datasets_L07.Rda")
 allStocks
+colnames(allStocks)
+# [1] "Date"   "Open"   "High"   "Low"    "Close"  "Volume" "Stock"
 nrow(allStocks)
+# 1621
+
 class(allStocks)
+# [1] "data.frame"
+
+str(allStocks)
+
 allStocksDT <-data.table(allStocks)
+
 class(allStocksDT)
+# [1] "data.table" "data.frame"
+
 str(allStocksDT) # structure of x
+
 allStocksDT[1:5][Stock == "bbby"]
 allStocksDT[1:5, "Stock"]
 allStocksDT[1:5, .(Stock)]
 # Finally we can use the by argument to do calculations by group. Here we
 # calculate mean and SD of Open by levels of Stock:
-allStocksDT[,.(meanOpen = mean(Open), sdOpen = sd(Open)), by = .(Stock)]
+allStocksDT[, .(meanOpen = mean(Open), sdOpen = sd(Open)), by = .(Stock)] # .() equals list()
 
-#The function is ":=" (read "colon equals"). It 
+
+#The function is ":=" (read "colon equals"). It
 # updates or adds column(s) by reference. That is, it makes no copies of any
 # part of memory at all. This can be very efficient for large data sets.
 allStocksDT[, Day := weekdays(Date)]
 # Remove the column we created:
 allStocksDT[, Day := NULL]
+
+
+
+###
+grep("[a-z]", letters)
+
+txt <- c("arm","foot","lefroo", "bafoobar")
+if(length(i <- grep("foo", txt)))
+  cat("'foo' appears at least once in\n\t", txt, "\n")
+i # 2 and 4
+txt[i]
+# [1] "foot"     "bafoobar"
+
+## Double all 'a' or 'b's;  "\" must be escaped, i.e., 'doubled'
+gsub("([ab])", "\\1_\\1_", "abc and ABC")
+
+txt <- c("The", "licenses", "for", "most", "software", "are",
+         "designed", "to", "take", "away", "your", "freedom",
+         "to", "share", "and", "change", "it.",
+         "", "By", "contrast,", "the", "GNU", "General", "Public", "License",
+         "is", "intended", "to", "guarantee", "your", "freedom", "to",
+         "share", "and", "change", "free", "software", "--",
+         "to", "make", "sure", "the", "software", "is",
+         "free", "for", "all", "its", "users")
+( i <- grep("[gu]", txt) ) # indices
+stopifnot( txt[i] == grep("[gu]", txt, value = TRUE) )
+
+## Note that in locales such as en_US this includes B as the
+## collation order is aAbBcCdEe ...
+(ot <- sub("[b-e]",".", txt))
+txt[ot != gsub("[b-e]",".", txt)]#- gsub does "global" substitution
+
+txt[gsub("g","#", txt) !=
+      gsub("g","#", txt, ignore.case = TRUE)] # the "G" words
+
+regexpr("en", txt)
+
+gregexpr("e", txt)
+
+## Using grepl() for filtering
+## Find functions with argument names matching "warn":
+findArgs <- function(env, pattern) {
+  nms <- ls(envir = as.environment(env))
+  nms <- nms[is.na(match(nms, c("F","T")))] # <-- work around "checking hack"
+  aa <- sapply(nms, function(.) { o <- get(.)
+  if(is.function(o)) names(formals(o)) })
+  iw <- sapply(aa, function(a) any(grepl(pattern, a, ignore.case=TRUE)))
+  aa[iw]
+}
+findArgs("package:base", "warn")
+
+## trim trailing white space
+str <- "Now is the time      "
+sub(" +$", "", str)  ## spaces only
+## what is considered 'white space' depends on the locale.
+sub("[[:space:]]+$", "", str) ## white space, POSIX-style
+## what PCRE considered white space changed in version 8.34: see ?regex
+sub("\\s+$", "", str, perl = TRUE) ## PCRE-style white space
+
+## capitalizing
+txt <- "a test of capitalizing"
+gsub("(\\w)(\\w*)", "\\U\\1\\L\\2", txt, perl=TRUE)
+gsub("\\b(\\w)",    "\\U\\1",       txt, perl=TRUE)
+
+txt2 <- "useRs may fly into JFK or laGuardia"
+gsub("(\\w)(\\w*)(\\w)", "\\U\\1\\E\\2\\U\\3", txt2, perl=TRUE)
+sub("(\\w)(\\w*)(\\w)", "\\U\\1\\E\\2\\U\\3", txt2, perl=TRUE)
+
+## named capture
+notables <- c("  Ben Franklin and Jefferson Davis",
+              "\tMillard Fillmore")
+# name groups 'first' and 'last'
+name.rex <- "(?<first>[[:upper:]][[:lower:]]+) (?<last>[[:upper:]][[:lower:]]+)"
+(parsed <- regexpr(name.rex, notables, perl = TRUE))
+gregexpr(name.rex, notables, perl = TRUE)[[2]]
+parse.one <- function(res, result) {
+  m <- do.call(rbind, lapply(seq_along(res), function(i) {
+    if(result[i] == -1) return("")
+    st <- attr(result, "capture.start")[i, ]
+    substring(res[i], st, st + attr(result, "capture.length")[i, ] - 1)
+  }))
+  colnames(m) <- attr(result, "capture.names")
+  m
+}
+parse.one(notables, parsed)
+
+## Decompose a URL into its components.
+## Example by LT (http://www.cs.uiowa.edu/~luke/R/regexp.html).
+x <- "http://stat.umn.edu:80/xyz"
+m <- regexec("^(([^:]+)://)?([^:/]+)(:([0-9]+))?(/.*)", x)
+m
+regmatches(x, m)
+## Element 3 is the protocol, 4 is the host, 6 is the port, and 7
+## is the path.  We can use this to make a function for extracting the
+## parts of a URL:
+URL_parts <- function(x) {
+  m <- regexec("^(([^:]+)://)?([^:/]+)(:([0-9]+))?(/.*)", x)
+  parts <- do.call(rbind,
+                   lapply(regmatches(x, m), `[`, c(3L, 4L, 6L, 7L)))
+  colnames(parts) <- c("protocol","host","port","path")
+  parts
+}
+URL_parts(x)
+
+## There is no gregexec() yet, but one can emulate it by running
+## regexec() on the regmatches obtained via gregexpr().  E.g.:
+pattern <- "([[:alpha:]]+)([[:digit:]]+)"
+s <- "Test: A1 BC23 DEF456"
+lapply(regmatches(s, gregexpr(pattern, s)),
+       function(e) regmatches(e, regexec(pattern, e)))
+
+
+
+####
+x <- 1:12
+m <- matrix(1:6, nrow = 2, dimnames = list(c("a", "b"), LETTERS[1:3]))
+li <- list(pi = pi, e = exp(1))
+x[10]                 # the tenth element of x
+x <- x[-1]            # delete the 1st element of x
+m[1,]                 # the first row of matrix m
+m[1, , drop = FALSE]  # is a 1-row matrix
+m[,c(TRUE,FALSE,TRUE)]# logical indexing
+m[cbind(c(1,2,1),3:1)]# matrix numeric index
+ci <- cbind(c("a", "b", "a"), c("A", "C", "B"))
+m[ci]                 # matrix character index
+m <- m[,-1]           # delete the first column of m
+li[[1]]               # the first element of list li
+y <- list(1, 2, a = 4, 5)
+y[c(3, 4)]            # a list containing elements 3 and 4 of y
+y$a                   # the element of y named a
+
+y <- list(l1 = c("a", "b", "c"), l2 = 1:3, l3 = LETTERS[10:12])
+
+y
+## non-integer indices are truncated:
+(i <- 3.999999999) # "4" is printed
+(1:5)[i]  # 3
+
+## named atomic vectors, compare "[" and "[[" :
+nx <- c(Abc = 123, pi = pi)
+nx[1] ; nx["pi"] # keeps names, whereas "[[" does not:
+nx[[1]] ; nx[["pi"]]
+
+## recursive indexing into lists
+z <- list(a = list(b = 9, c = "hello"), d = 1:5)
+unlist(z)
+z[[c(1, 2)]]
+z[[c(1, 2, 1)]]  # both "hello"
+z[[c("a", "b")]] <- "new"
+unlist(z)
+
+## check $ and [[ for environments
+e1 <- new.env()
+e1$a <- 10
+e1[["a"]]
+e1[["b"]] <- 20
+e1$b
+ls(e1)
+
+## partial matching - possibly with warning :
+stopifnot(identical(li$p, pi))
+op <- options(warnPartialMatchDollar = TRUE)
+stopifnot( identical(li$p, pi), #-- a warning
+           inherits(tryCatch (li$p, warning = identity), "warning"))
+## revert the warning option:
+if(is.null(op[[1]])) op[[1]] <- FALSE; options(op)
+
+
+
 
 
