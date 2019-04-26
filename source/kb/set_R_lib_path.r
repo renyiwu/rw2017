8276490223fc0a707ca383c9e2a4a3d453b42a03
R Wu <renyiwu@gmail.com>
2019-04-26 14:43:44 -0400

    4-26 2019 v1

---
+++ source/kb/set_R_lib_path.r
@@ -0,0 +1,27 @@
+# How to set shared lib folder for all R versions
+# April 26, 2019
+
+# Windows
+
+1. Create a new folder, eg. C:\users\abc\Rlib. Optionally, copy old packages to this folder.
+
+2. open any text editor and save "R_LIBS_USER=/home/abc/Rlib" in a file 
+named .Renviron under your HOME directory (usually C:/users/abc/Documents) 
+# the correct path can be obtained by R command path.expand("~")
+
+# or go to  Start --> Control Panel --> User Accounts --> Change my environmental variables, 
+# add a new entry, R_LIBS_USER - C:\users\abc\Rlib ( note backslashes in Windows)
+
+3. Within R, run
+update.packages(checkBuilt = TRUE, ask = FALSE)	
+
+
+
+# Linux
+ 
+1. Create a new folder, eg. /home/abc/R/library and optionally copy old packages to this folder
+2. in shell run:
+  cat "R_LIBS_USER=/home/abc/R/library" >/home/abc/.Renviron
+
+3. Within R, run
+update.packages(checkBuilt = TRUE, ask = FALSE)
