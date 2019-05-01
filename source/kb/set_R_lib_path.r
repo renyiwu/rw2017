97211dd2964951594d0c5bd8201b84f31ae3f4bb
renyiwu <renyiwu@gmail.com>
2019-05-01 13:40:13 -0400

    change cat to echo

--- source/kb/set_R_lib_path.r
+++ source/kb/set_R_lib_path.r
@@ -5,23 +5,23 @@
 
 1. Create a new folder, eg. C:\users\abc\Rlib. Optionally, copy old packages to this folder.
 
-2. open any text editor and save "R_LIBS_USER=C:\users\abc\Rlib" in a file 
-named .Renviron under your HOME directory (usually C:\users\abc\Documents) 
+2. open any text editor and save "R_LIBS_USER=C:\users\abc\Rlib" in a file
+named .Renviron under your HOME directory (usually C:\users\abc\Documents)
 # the correct path can be obtained by R command path.expand("~")
 
-# or go to  Start --> Control Panel --> User Accounts --> Change my environmental variables, 
+# or go to  Start --> Control Panel --> User Accounts --> Change my environmental variables,
 # add a new entry, R_LIBS_USER - C:\users\abc\Rlib ( note backslashes in Windows)
 
 3. Within R, run
-update.packages(checkBuilt = TRUE, ask = FALSE)	
+update.packages(checkBuilt = TRUE, ask = FALSE)
 
 
 
 # Linux
- 
+
 1. Create a new folder, eg. /home/abc/R/library and optionally copy old packages to this folder
-2. in shell run:
-  cat "R_LIBS_USER=/home/abc/R/library" >/home/abc/.Renviron
+2. in terminal run:
+  echo "R_LIBS_USER=/home/abc/R/library" >/home/abc/.Renviron
 
 3. Within R, run
 update.packages(checkBuilt = TRUE, ask = FALSE)
