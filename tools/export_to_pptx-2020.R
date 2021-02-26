3fbca0521f72b45fb3e8e24feae68236f2c9697d
renyiwu <renyiwu@gmail.com>
2021-02-26 09:04:30 -0500

    fghtb

--- tools/export_to_pptx-2020.R
+++ tools/export_to_pptx-2020.R
@@ -10,20 +10,43 @@
 
 # Sept 2020 new dml object
 
-create_pptx <- function(plot, path, width = 6, height = 6, pointsize = 12){
+create_pptx <- function(plot, path, w=10, h = 7.5, l = 0.05, t = 0.05 ){
   library("officer")
   library("magrittr")
   library("rvg")
-  if(!file.exists(path)) {
-    out <- read_pptx()
-  } else {
+  
+ if(file.exists(path)) {
+   if (!file.exists(paste0("~$", path))){
     out <- read_pptx(path)
-  }
+   }
+  } 
+  else {
+    path <- paste0("temp_", path)
+     out <- read_pptx(path)
+   }
 
-  out %>%
-    add_slide(layout = "Title and Content", master = "Office Theme") %>%
-    ph_with(dml(code = print(plot)), # rvg:::ph_with.dml(dml(code = print(plot)),
-                      ph_location_fullsize()
-                      ) %>%
-    print(target = path)
+   out %>%
+     add_slide(layout = "Title and Content", master = "Office Theme") %>%
+     ph_with(dml(code = print(plot)), 
+             # rvg:::ph_with.dml(dml(code = print(plot)),
+             # ph_location_fullsize()
+             ph_location(left = l, top = t, width = w,  height = h, newlabel = "", bg = NULL, rotation = NULL)
+             ) %>%
+     print(target = path)
+
 }
+
+
+## to use in a loop
+#1. first create an object outside the loop
+# pptx_out <- read_pptx()
+
+#2. then put this in the loop
+pptx_out %>%
+add_slide(layout = "Title and Content", master = "Office Theme") %>%
+ph_with(dml(code = print(plot)), # rvg:::ph_with.dml(dml(code = print(plot)),
+        ph_location_fullsize()
+)
+
+#3. finally write to file
+# print(pptx_out, path)
