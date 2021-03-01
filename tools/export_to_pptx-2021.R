ea6a30646acd83fbf5470badd2464c92d1cb12bd
renyiwu <renyiwu@gmail.com>
2021-02-28 19:17:24 -0500

    add support for png and tiff output

--- tools/export_to_pptx-2021.R
+++ tools/export_to_pptx-2021.R
@@ -1,6 +1,8 @@
 # Functions for importing (vectors) figures to Powerpoint slides.
 # R Wu. Oct 2018
-# revised Nov 2020
+# revised Feb 2021
+# add support for png and tiff
+
 
 # install.packages("officer")
 # install.packages("magrittr") #the package for function "%>%"
@@ -10,10 +12,11 @@
 
 # Sept 2020 new dml object
 
-create_pptx <- function(plot, path, width = 8, height = 6, left=0.05, top=0.05){
+create_pptx <- function(plot, path, width = 8, height = 6, left=0.05, top=0.05, png=TRUE, tiff=FALSE, res=100){
   library("officer")
   library("magrittr")
   library("rvg")
+  library("stringr")
   if(!file.exists(path)) {
     out <- read_pptx()
   } else {
@@ -27,4 +30,33 @@ create_pptx <- function(plot, path, width = 8, height = 6, left=0.05, top=0.05){
             ph_location(lef=left, top=top, width=width, height = height)
                       ) %>%
     print(target = path)
+  
+  ### create png ### 
+  # check series numbers.
+  if (png){
+  n <- 1
+  while (n){
+    file <- paste0(substr(path,1, nchar(path)-4), str_pad(n, 3, "left", "0"), ".png")
+    if (! file.exists(file))
+      break
+    n = n + 1
+  }
+  png(file, width=width, height = height, units = "in", res = res)
+  print(plot)
+  dev.off()
+  }
+  
+  ### create tiff
+  if (tiff){
+    n <- 1
+    while (n){
+      file <- paste0(substr(path,1, nchar(path)-4), str_pad(n, 3, "left", "0"), ".tif")
+      if (! file.exists(file))
+        break
+      n = n + 1
+    }
+    tiff(file, width=width, height = height, units = "in", res = res)
+    print(plot)
+    dev.off()
+  }
 }
