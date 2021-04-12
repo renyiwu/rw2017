53db8118e474a7ff877027b28fc6138bc4c6a88d
rw sop1482 <renyiwu@gmail.com>
2021-04-12 12:48:14 -0400

    fix url

--- NGS.Shiny/VERSION.R
+++ NGS.Shiny/VERSION.R
@@ -1,4 +1,4 @@
-UPDATE_VERSION <- "v0.2"
+UPDATE_VERSION <- "v0.1"
 UPDATE_DATE <- "April 12, 2021"
 UPDATE_REPO <- "renyiwu/rw2017"
-UPDATE_URL <- "https://raw.githubusercontent.com/renyiwu/rw2017/NGS.Shiny/master/VERSION.R"
+UPDATE_URL <- "https://raw.githubusercontent.com/renyiwu/rw2017/master/NGS.Shiny/VERSION.R"
